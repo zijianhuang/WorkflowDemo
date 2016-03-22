0ebdd69d8d4768b4ef10d3c15cda22087a17bdd1
admin <admin@fonlow.com>
2016-03-23 06:55:40 +1000

    more examples

--- BasicTests/WorkflowServiceHostTests.cs
+++ BasicTests/WorkflowServiceHostTests.cs
@@ -12,33 +12,58 @@ using System.Activities.DurableInstancing;
 using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceModel.Activities;
+using System.ServiceModel.Activities.Description;
 
 namespace BasicTests
 {
     public class WorkflowServiceHostTests
     {
-            //const string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=InstanceStore;Integrated Security=True;Asynchronous Processing=True";
-            const string hostBaseAddress = "net.tcp://localhost/CountingService";
+        const string connectionString = "Server =localhost; Initial Catalog = WF; Integrated Security = SSPI";
+        const string hostBaseAddress = "net.tcp://localhost/CountingService";
         [Fact]
-        public void TestOpenHostWithoutStoreThrows()
+        public void TestOpenHostWithoutContractImpThrows()
         {
             // Create service host.
-            WorkflowServiceHost host = new WorkflowServiceHost(new Plus(), new Uri(hostBaseAddress));
+            using (WorkflowServiceHost host = new WorkflowServiceHost(new Plus(), new Uri(hostBaseAddress)))
+            {
 
-            // Add service endpoint.
-            host.AddServiceEndpoint("ICountingWorkflow", new NetTcpBinding(), "");
-
-            // Open service host.
-            host.Open();
-
+                Assert.Throws<InvalidOperationException>(()
+                    => host.AddServiceEndpoint("ICountingWorkflow", new NetTcpBinding(), ""));
+            }
         }
-    }
 
-        [ServiceContract]
-        public interface ICountingWorkflow
+        [Fact]
+        public void TestOpenHostWithWrongStoreThrows()
         {
-            [OperationContract(IsOneWay = true)]
-            void start();
-        }
+            // Create service host.
+            WorkflowServiceHost host = new WorkflowServiceHost(new Microsoft.Samples.BuiltInConfiguration.CountingWorkflow(), new Uri(hostBaseAddress));
+            
 
+                // Add service endpoint.
+                host.AddServiceEndpoint("ICountingWorkflow", new NetTcpBinding(), "");
+
+            SqlWorkflowInstanceStoreBehavior instanceStoreBehavior = new SqlWorkflowInstanceStoreBehavior("Server =localhost; Initial Catalog = WFXXX; Integrated Security = SSPI");
+            instanceStoreBehavior.HostLockRenewalPeriod = new TimeSpan(0, 0, 5);
+            instanceStoreBehavior.RunnableInstancesDetectionPeriod = new TimeSpan(0, 0, 2);
+            instanceStoreBehavior.InstanceCompletionAction = InstanceCompletionAction.DeleteAll;
+            instanceStoreBehavior.InstanceLockedExceptionAction = InstanceLockedExceptionAction.AggressiveRetry;
+            instanceStoreBehavior.InstanceEncodingOption = InstanceEncodingOption.GZip;
+            host.Description.Behaviors.Add(instanceStoreBehavior);
+
+            var ex = Assert.Throws<CommunicationException>(()
+                => host.Open(TimeSpan.FromSeconds(2)));
+
+            Assert.NotNull(ex.InnerException);
+            Assert.Equal(typeof(System.Runtime.DurableInstancing.InstancePersistenceCommandException), ex.InnerException.GetType());
+            Assert.Equal(CommunicationState.Faulted, host.State);//so can't be disposed.
+        }
     }
+
+    [ServiceContract]
+    public interface ICountingWorkflow
+    {
+        [OperationContract(IsOneWay = true)]
+        void start();
+    }
+
+}
