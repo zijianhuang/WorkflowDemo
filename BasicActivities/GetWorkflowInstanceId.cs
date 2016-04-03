bce43188144c74a1e19a177240e3adc26aa5a41e
admin <admin@fonlow.com>
2016-04-04 00:14:06 +1000

    slight changes

---
+++ BasicActivities/GetWorkflowInstanceId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Fonlow.Activities
+{
+
+    public sealed class GetWorkflowInstanceId : CodeActivity<Guid>
+    {
+        protected override Guid Execute(CodeActivityContext context)
+        {
+            return context.WorkflowInstanceId;
+        }
+    }
+}
