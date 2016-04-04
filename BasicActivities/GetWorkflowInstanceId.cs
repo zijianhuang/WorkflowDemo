03ee959333b35abaa7f062a557e9d93feeb746cc
admin <admin@fonlow.com>
2016-04-05 07:12:58 +1000

    minor changes

--- BasicActivities/GetWorkflowInstanceId.cs
+++ BasicActivities/GetWorkflowInstanceId.cs
@@ -6,7 +6,9 @@ using System.Activities;
 
 namespace Fonlow.Activities
 {
-
+    /// <summary>
+    /// For workflow that need to return the InstanceId to client.
+    /// </summary>
     public sealed class GetWorkflowInstanceId : CodeActivity<Guid>
     {
         protected override Guid Execute(CodeActivityContext context)
